c5ce01238d1455c143d1f1f2a0210d52e0fd7dae
Wonday <wonday@wonday.org>
2017-06-10 16:45:30 +0800

    add aliyun push ios sdk

---
+++ ios/RCTAliyunPush/AliyunPushManager.h
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2017-present, Wonday (@wonday.org)
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTLog.h>
+
+@interface AliyunPushManager : RCTEventEmitter <RCTBridgeModule>
+
+@end
---
+++ ios/RCTAliyunPush/AliyunPushManager.m
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2017-present, Wonday (@wonday.org)
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
+#import "AliyunPushManager.h"
+
+@implementation AliyunPushManager
+
+
+RCT_EXPORT_MODULE(AliyunPush);
+
+@end
